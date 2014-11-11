0fe5a914cf9a548d13ddaeb074e664fc02b88713
bagnz0r <bagnohax@gmail.com>
2014-11-11 16:53:43 +0100

    Build for x64 on linux

--- build.sh
+++ build.sh
@@ -27,18 +27,18 @@ if [ $1 == "linux" ]; then
 	ext="so"
 	libs="-Ldependencies/linux/bass_aac -Ldependencies/linux/bass_alac -lbass_aac -lbass_alac"
 	headers="-Idependencies/linux/bass_aac -Idependencies/linux/bass_alac"
-	arch="-m32"
+	arch=""
 	std="-std=gnu11"
 
-	cp -v dependencies/linux/bass/libbass.so output/libbass.so
-	cp -v dependencies/linux/bass_fx/libbass_fx.so output/libbass_fx.so
-	cp -v dependencies/linux/bassflac/libbassflac.so output/libbassflac.so
-	cp -v dependencies/linux/bass_aac/libbass_aac.so output/libbass_aac.so
-	cp -v dependencies/linux/bass_alac/libbass_alac.so output/libbass_alac.so
-	cp -v dependencies/linux/basswv/libbasswv.so output/libbasswv.so
-	cp -v dependencies/linux/bass_ape/libbass_ape.so output/libbass_ape.so
-	cp -v dependencies/linux/bass_mpc/libbass_mpc.so output/libbass_mpc.so
-	cp -v dependencies/linux/tags/libtags.so output/libtags.so
+	cp -v dependencies/linux/bass/x64/libbass.so output/libbass.so
+	cp -v dependencies/linux/bass_fx/x64/libbass_fx.so output/libbass_fx.so
+	cp -v dependencies/linux/bassflac/x64/libbassflac.so output/libbassflac.so
+	cp -v dependencies/linux/bass_aac/x64/libbass_aac.so output/libbass_aac.so
+	cp -v dependencies/linux/bass_alac/x64/libbass_alac.so output/libbass_alac.so
+	cp -v dependencies/linux/basswv/x64/libbasswv.so output/libbasswv.so
+	cp -v dependencies/linux/bass_ape/x64/libbass_ape.so output/libbass_ape.so
+	cp -v dependencies/linux/bass_mpc/x64/libbass_mpc.so output/libbass_mpc.so
+	cp -v dependencies/linux/tags/x64/libtags.so output/libtags.so
 
 	cp -v ichigo-audio.c ichigo-audio.build.c
 fi
